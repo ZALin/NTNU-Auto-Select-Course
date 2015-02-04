2907f9fd50fc21b08d7115deec6b6f2e459dddfb
ZALin <andy03031193+github@gmail.com>
2015-02-03 21:28:44 -0800

    can try to auto login

--- autoselect.py
+++ autoselect.py
@@ -1,4 +1,4 @@
-# -*- -*-
+#coding: utf-8
 #http://www.lfd.uci.edu/~gohlke/pythonlibs/
 #sudo apt-get install tesseract-ocr
 #pip install PIL 
@@ -11,6 +11,7 @@ from pytesser import *
 import requests
 import shutil
 import re
+import getpass
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -41,65 +42,73 @@ login_header= {
 userid = '40040001S'
 password = 'XDDDDDDDD'
 
+Sucess_login = False
+userid = raw_input('Please input student ID: ')
+password = getpass.getpass('Please input Password: ')
+
+while Sucess_login == False :
+	logincheck_url = 'http://cos4.ntnu.edu.tw/AasEnrollStudent/LoginCheckCtrl?language=TW'
+	response = requests.get(logincheck_url, headers=logincheck_header)
+	JSESSIONID = response.cookies['JSESSIONID']
+	print "cookies :" + JSESSIONID
+
+	m = re.search(r'url:\'LoginCheckCtrl\?action=login&id=\' \+ \'([0-9a-zA-Z]+)\',' , response.content)
+	login_id = m.group(1)
+	print "id : " + login_id
+
+	Captcha_url = 'http://cos4.ntnu.edu.tw/AasEnrollStudent/RandImage'
+	Captcha_res = requests.get(Captcha_url, stream=True)
+	with open(os.path.join(script_dir, 'img'), 'wb') as out_file:
+	    shutil.copyfileobj( Captcha_res.raw, out_file )
+	out_file.close
+	del Captcha_res
+	#img_file = cStringIO.StringIO(response.read())   
+	#image = Image.open(img_file)
+	#image = Image.open("C:/Users/Andy/git_repo/a/fnord.tif")
+	#image = Image.open( open("~/auto_s/fnord.tif", 'rb'))
+	#import os.path
+
+	#script_dir = os.path.dirname(os.path.abspath(__file__))
+	print script_dir
+	image = Image.open(os.path.join(script_dir, 'img'))
+
+	#image = Image.open( r"./img")
+
+	Captcha_code = image_to_string(image).strip()
+	print "Captcha_code:" + Captcha_code
+	#a = image_file_to_string('img')
+	#print type(a)
+
+
+	login_header['Cookie'] = 'JSESSIONID=' + JSESSIONID 
+	print login_header
+
+	login_payload = {'userid': userid , 'password': password , 'validateCode' : Captcha_code , 'checkTW' : '1' }
+	print login_payload
 
 
 
-logincheck_url = 'http://cos4.ntnu.edu.tw/AasEnrollStudent/LoginCheckCtrl?language=TW'
-response = requests.get(logincheck_url, headers=logincheck_header)
-JSESSIONID = response.cookies['JSESSIONID']
-print "cookies :" + JSESSIONID
-
-m = re.search(r'url:\'LoginCheckCtrl\?action=login&id=\' \+ \'([0-9a-zA-Z]+)\',' , response.content)
-login_id = m.group(1)
-print "id : " + login_id
-
-Captcha_url = 'http://cos4.ntnu.edu.tw/AasEnrollStudent/RandImage'
-Captcha_res = requests.get(Captcha_url, stream=True)
-with open(os.path.join(script_dir, 'img'), 'wb') as out_file:
-    shutil.copyfileobj( Captcha_res.raw, out_file )
-out_file.close
-del Captcha_res
-#img_file = cStringIO.StringIO(response.read())   
-#image = Image.open(img_file)
-#image = Image.open("C:/Users/Andy/git_repo/a/fnord.tif")
-#image = Image.open( open("~/auto_s/fnord.tif", 'rb'))
-#import os.path
-
-#script_dir = os.path.dirname(os.path.abspath(__file__))
-print script_dir
-image = Image.open(os.path.join(script_dir, 'img'))
-
-	
+	login_url = 'http://cos4.ntnu.edu.tw/AasEnrollStudent/LoginCheckCtrl?action=login&id='+login_id
 
 
-#image = Image.open( r"./img")
+	response = requests.post(login_url, data=login_payload ,headers=login_header)
+	print "======================"
+	print response.status_code
+	print response.headers
+	print response
+	#q = 
+	#print response.json()
+	print response.text
+	print response.content
+	print "======================"
 
-
-
-Captcha_code = image_to_string(image).strip()
-print "Captcha_code:" + Captcha_code
-#a = image_file_to_string('img')
-#print type(a)
-
-
-login_header['Cookie'] = 'JSESSIONID=' + JSESSIONID 
-print login_header
-
-login_payload = {'userid': userid , 'password': password , 'validateCode' : Captcha_code , 'checkTW' : '1' }
-print login_payload
-
-
-
-login_url = 'http://cos4.ntnu.edu.tw/AasEnrollStudent/LoginCheckCtrl?action=login&id='+login_id
-
-
-response = requests.post(login_url, data=login_payload ,headers=login_header)
-print "======================"
-print response.status_code
-print response.headers
-print response
-#q = 
-#print response.json()
-print response.text
-print response.content
-print "======================"
+	if 'success:true' in response.content :
+		Sucess_login = True
+		print "Success Login"
+	elif '驗證碼錯誤' in response.content :
+		print "驗證碼錯誤... auto try again..."
+	else :
+		print 'Error with ' + response.content[19:-1]
+		userid = raw_input('Please input student ID: ')
+		password = getpass.getpass('Please input Password: ')
+print "Sucess_login"
